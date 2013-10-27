229728f77a30a63043469f559591c436b9fffc03
Marco Pehla <Marco.Pehla@googlemail.com>
2013-10-27 18:47:06 +0100

    added first lines of code, but impl. is still not finished

--- src/main/java/de/tecgen/raw/tools/dng/CinemaDNG.java
+++ src/main/java/de/tecgen/raw/tools/dng/CinemaDNG.java
@@ -2,6 +2,9 @@ package de.tecgen.raw.tools.dng;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Purpose of this class is to allow basic operations for Cinema DNG clips. 
@@ -14,6 +17,8 @@ import java.io.FileNotFoundException;
  * @date 2013-10-27
  */
 public class CinemaDNG {
+	
+	private final static String ExtentionCinemaDNG = "dng";
 
 	/**
 	 * 
@@ -38,13 +43,38 @@ public class CinemaDNG {
 	 */
 	private static boolean isCinemaDNGClip(File clip) {
 		if(clip.isDirectory()) {
+			final String clipDirectoryName = clip.getName();
+			
+			// get all *.dng files in the directory
+			List<File> dngFiles = Arrays.asList(clip.listFiles(new FilenameFilter() {
+				public boolean accept(File directory, String fileName) {
+					return fileName.endsWith("."+ExtentionCinemaDNG.toLowerCase()) 
+							|| fileName.endsWith("."+ExtentionCinemaDNG.toUpperCase());
+				}	
+			}));
+			
 			// the directory contains at least one DNG-file ...
 			// e.g. "2013-10-27_1812_(M27-1812)_C0000_00000.dng"
+			if(dngFiles.size() < 1) {
+				// stop when no matching DNG-file has been found
+				return false;
+			} else {
+				// ... with suffix _00000 
+				for(File dngFile : dngFiles) {
+					// all DNG-files need to have the same file name (except for the suffix)
+					// and moreover the directory shares this name too!
+					
+				}
+				
+				
+				
+				
+			}
 			
-			// ... with suffix _00000 
 			
-			// all DNG-files need to have the same file name (except for the suffix)
-			// the directory shares this name too!
+			
+			
+			
 			
 			//FIXME 
 			return false;
