f402df468de7db8c8ca2fe1c450a9aace2cc63d5
Marco Pehla <Marco.Pehla@googlemail.com>
2013-10-27 16:35:54 +0100

    fixed a typo in the script, now its working again as expected

--- copyRawFilesFromCard.sh
+++ copyRawFilesFromCard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source user.cfg
-rsync --progress "$source_folder_raw/*.R*" .
+rsync --progress $source_folder_raw/*.R* .
 
 
 #rsync --progress /Volumes/EOS_DIGITAL/DCIM/100CANON/*.R* .
