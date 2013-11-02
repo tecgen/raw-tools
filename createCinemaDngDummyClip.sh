abc41201d95b4960fcf7ba93b8b0ff6314f6f879
tecgen <tecgen@marcos-mbp.fritz.box>
2013-11-02 16:18:28 +0100

    unneccesary

--- createCinemaDngDummyClip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-clip='M04_1823_C0000'
-mkdir $clip
-echo emtpy > $clip/$clip_000000.dng
-#touch $clip/$clip_000000.dng
-#touch "$clip/$clip_000001.dng"
-#touch "$clip/$clip_000002.dng"
-#touch "$clip/$clip_000003.dng"
